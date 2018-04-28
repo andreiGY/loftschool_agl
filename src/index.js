34e782bbb56bfa7406df3dea909116e54aa5618a
Gladarenko Andrei <a.gladarenko@gmail.com>
2018-04-28 15:20:33 +0300

    первое домашнее задание без 6-го рабочего

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {
+function returnFirstArgument(a) {
+  return a;
 }
 
 /*
@@ -29,7 +30,8 @@ function returnFirstArgument() {
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {
+function sumWithDefaults(a, b = 100) {
+  return a + b;
 }
 
 /*
@@ -41,6 +43,8 @@ function sumWithDefaults(a, b) {
    returnFnResult(() => 'привет') вернет 'привет'
  */
 function returnFnResult(fn) {
+  var f = fn();
+  return f;
 }
 
 /*
@@ -56,7 +60,9 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {
+function returnCounter(number = 0) {
+  
+  return function() {return number = number + 1;};
 }
 
 /*
@@ -69,6 +75,9 @@ function returnCounter(number) {
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
 function returnArgumentsArray() {
+   var argsArray = [].slice.call(arguments);
+
+  return argsArray;
 }
 
 /*
@@ -87,6 +96,12 @@ function returnArgumentsArray() {
    console.log(newSum()) выведет 6
  */
 function bindFunction(fn) {
+  var a = [].slice.call(arguments);
+  return function(a) {
+    //fn.apply(context, a);
+    
+  }
+
 }
 
 export {
